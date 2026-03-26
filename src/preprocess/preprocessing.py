70f1376bcdbf27053af3920407a2df8be3f28059
dnhfan <dinhan.0321@gmail.com>
2026-03-26 14:50:43 +0700

    feat: preprocessing package to handle unbalance class

---
+++ src/preprocess/preprocessing.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from imblearn.over_sampling import SMOTE
+
+
+def load_and_preprocess(filepath, target_index=0, save_path=None):
+    """
+    Pipeline for clean and balances the dataset:
+        - filepath: data filepath
+        - target_index: index of target collume
+    """
+
+    # 1. load data
+    df = pd.read_csv(filepath)
+
+    # 2. split X and Y
+    y = df.iloc[:, target_index]
+
+    x = df.drop(columns=df.columns[target_index])
+
+    # 3. data balancing with SMOTE!
+    smote = SMOTE(random_state=42)
+
+    x_resampled, y_resampled = smote.fit_resample(x, y)  # type: ignore
+
+    x_final = pd.DataFrame(x_resampled, columns=x.columns)
+
+    print(f"󰄲 Preprocessed file: {filepath}")
+    print(f"󰓫 X.Shape before preprocess: {x.shape}")
+    print(f"󰓫 X.Shape after preprocess: {x_final.shape}")
+
+    if save_path:
+        df_processed = x_final.copy()
+
+        df_processed.insert(0, "V1", y_resampled)
+
+        df_processed.to_csv(save_path, index=False)
+
+        print(f" Saved preprocess data in {save_path}")
+
+    return x_final, y_resampled
+
+
+# in file test function
+if __name__ == "__main__":
+    raw_path = "data/raw/Lung_cancer.csv"
+    save_path = "data/processed/Lung_cancer_preprocessed.csv"
+
+    x_train, y_train = load_and_preprocess(filepath=raw_path, save_path=save_path)
