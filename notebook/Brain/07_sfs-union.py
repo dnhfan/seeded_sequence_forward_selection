c1aac7751fee6aef4e28d4613f4ae89a90b56e66
dnhfan <dinhan.0321@gmail.com>
2026-04-09 14:02:21 +0700

    feat: new phase 05 - Union set and refactor sfs-union with new class

--- notebook/Brain/07_sfs-union.py
+++ notebook/Brain/07_sfs-union.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
+import pandas
+
 # Tính toán lùi về 2 cấp thư mục: sfs.py -> Tumors9 -> notebook -> wrapper-w-filter (Gốc)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 from src.config import ProjectPath
-from src.utils import create_union_features
-from src.wrapper.wrapper_selector import WrapperSelector
+from src.wrapper import SeededSFSSelector
 
 
 def main():
@@ -16,41 +17,26 @@ def main():
     data_name = "Brain"
     n_features = 50
 
-    valid_methods = [
-        "variance",
-        "correlation",
-        "chi_squared",
-        "mutual_information",
-        "anova_f_test",
-    ]
-
     path = ProjectPath(data_name=data_name, n_features=n_features)
 
-    voting_csv_name = f"top{n_features}_features_voting_20260304_0337.csv"
+    voting_csv_name = f"top{n_features}_features_voting.csv"
 
     # 2. Init WrapperSelector
-    wrapper = WrapperSelector(
+    wrapper = SeededSFSSelector(
         data_name=data_name,
-        valid_method=valid_methods,
         n_features=n_features,
         voting_csv_name=voting_csv_name,
         dataset_variant="union",
+        run_tag="3st_test",
     )
 
-    df = create_union_features(
-        data_name=data_name,
-        valid_method=valid_methods,
-        n_features=n_features,
-        filter_dir=str(path.filter_dir),
-        raw_path=str(path.raw_path),
-        ensemble_dir=str(path.ensemble_dir),
-    )
+    union_df = pandas.read_csv(path.ensemble_file("union"))
 
     df_final = wrapper.run_sfs(
-        df=df,
+        df=union_df,
         max_features=20,
         patience=5,
-        n_seeds=3,
+        n_seeds=1,
         model="dt",
         scoring="accuracy",
         cv=4,
