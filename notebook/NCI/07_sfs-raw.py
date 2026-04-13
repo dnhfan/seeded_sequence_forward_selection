3d67754587c7d25b1893adc7c87a466441ccc27d
dnhfan <dinhan.0321@gmail.com>
2026-04-13 10:18:17 +0700

    feat: full pipeline for NCI

--- notebook/NCI/07_sfs-raw.py
+++ notebook/NCI/07_sfs-raw.py
@@ -7,7 +7,7 @@ import pandas as pd
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 from src.config import ProjectPath
-from src.wrapper.wrapper_selector import WrapperSelector
+from src.wrapper import SeededSFSSelector
 
 
 def main():
@@ -15,28 +15,20 @@ def main():
 
     # 1. setup conf
     data_name = "NCI"
-    n_features = 60
-
-    valid_methods = [
-        "variance",
-        "correlation",
-        "chi_squared",
-        "mutual_information",
-        "anova_f_test",
-    ]
+    n_features = 50
 
     path = ProjectPath(data_name=data_name, n_features=n_features)
 
-    voting_csv_name = f"top60_features_voting_20260304_0950.csv"
+    voting_csv_name = f"top{n_features}_features_voting.csv"
 
     # 2. Init WrapperSelector
-    wrapper = WrapperSelector(
+    wrapper = SeededSFSSelector(
         data_name=data_name,
-        valid_method=valid_methods,
         n_features=n_features,
         voting_csv_name=voting_csv_name,
         using_timer=True,
-        unit="s",
+        unit="ms",
+        dataset_variant="raw",
     )
 
     df = pd.read_csv(path.raw_path)
@@ -46,7 +38,7 @@ def main():
         max_features=20,
         patience=3,
         n_seeds=1,
-        model="dt",
+        model="log",
         scoring="accuracy",
         cv=5,
     )
