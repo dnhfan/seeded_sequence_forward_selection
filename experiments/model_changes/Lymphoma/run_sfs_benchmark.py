cd0293724df47fff4c3643fbfb425e9c5d94522e
dnhfan <dinhan.0321@gmail.com>
2026-06-25 08:52:58 +0700

    feat: exprient scirpt

---
+++ experiments/model_changes/Lymphoma/run_sfs_benchmark.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+import pandas as pd
+
+from src.config import ProjectPath
+from src.wrapper import SeededSFSSelector
+
+
+def main():
+    data_name = "Lymphoma"
+
+    models = ["log", "dt", "rf", "svm"]
+
+    # [TÙY CHỈNH CHO TỪNG DATASET TẠI ĐÂY]
+    n_features = 50
+    cv_value = 4
+    max_features_sfs = 20
+    patience_sfs = 5
+    data_variant = "raw"
+
+    path = ProjectPath(data_name, n_features)
+    voting_csv_name = f"top{n_features}_features_voting.csv"
+
+    # Load data
+    try:
+        if data_variant == "union":
+            df = pd.read_csv(path.ensemble_file("union"))
+        else:
+            df = pd.read_csv(path.raw_path)
+    except FileNotFoundError as e:
+        print(f" Error: Not FileNotFoundError {e}")
+        return
+
+    print(f" Setup benchmark for dataset: {data_name} (Variant: {data_variant})")
+    for model in models:
+        print("\n Running SFS benchmark for model:", model.upper())
+
+        wrapper = SeededSFSSelector(
+            data_name=data_name,
+            n_features=n_features,
+            voting_csv_name=voting_csv_name,
+            dataset_variant=data_variant,
+            run_tag=f"benchmark_{model}_1seeds",
+            using_timer=True,
+            unit="ms",
+        )
+
+        wrapper.run_sfs(
+            df=df,
+            max_features=max_features_sfs,
+            n_seeds=1,
+            patience=patience_sfs,
+            cv=cv_value,
+            scoring="accuracy",
+            model=model,
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ experiments/model_changes/README.md
@@ -0,0 +1,19 @@
+# Changing model scenarios
+
+## Description
+
+- keeping all other parameters fixed.
+- we will change the model used in the sfs process to see how it affects the results.
+
+## Models to compare
+
+### Tree-based models
+
+1. Decision Tree
+2. Random Forest
+
+### Linear models
+
+1. Logistic Regression
+2. Linear Regression (L1 Regularizatioan / Lasso)
+3. Linear Support Vector Machine (SVM)
